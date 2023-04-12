8512bcde1912b397e1bcd17a4490c30f3233c81d
Pranjal Aggarwal <Pranjal.Aggarwal1@ibm.com>
2023-04-12 20:45:45 +0530

    Working

--- word2doc.py
+++ word2doc.py
@@ -2,6 +2,7 @@ import argparse
 from pathlib import Path
 import os
 import win32com.client
+import sys
 
 def convertToPdf(paths):
     word_instance = win32com.client.Dispatch("Word.Application")
@@ -10,11 +11,18 @@ def convertToPdf(paths):
     if paths['bulk']:
         print("Converting files from the given folder")
         for filepath in Path(paths['input']).glob('*.doc*'):
-            pdf_path = Path(paths['output'] / (str(filepath.stem) + ".pdf"))
-            document = word.Documents.Open(str(filepath))
+            pdf_path = Path(paths['output']) / (str(filepath.stem) + ".pdf")
+            document = word_instance.Documents.Open(str(filepath))
             document.SaveAs(str(pdf_path), FileFormat=wdFromatPDF)
-            doc.close(0)
+            document.Close(0)
     
+    else:
+        print("Converting ...")
+        filepath = Path(paths['input'])
+        pdf_path = Path(paths['output'])
+        document = word_instance.Documents.Open(str(filepath))
+        document.SaveAs(str(pdf_path), FileFormat=wdFromatPDF)
+        document.Close(0)
 
 def identify_path(in_path, out_path=None):
     # Resolve paths to handle relative paths
@@ -22,17 +30,22 @@ def identify_path(in_path, out_path=None):
     if out_path is not None:
         out_path = Path(out_path).resolve()
 
+    # print("in_path ", in_path)
+    # print("Folder ", in_path.is_dir())
+    # print("out_path ", out_path)
+    # print("Folder ", out_path.is_dir())
+
     paths = {}
     # Condition when the path is a file
     if os.path.isfile(in_path):
-        print("Path is a file.")
+        print("Identified input as a file")
         paths['bulk'] = False
         paths['input'] = str(in_path)
 
         if out_path and os.path.isdir(out_path):
             out_path = os.path.join(out_path, in_path.stem) + ".pdf"
         elif out_path:
-            print("Path does not exist")
+            print("Output path does not exist")
             sys.exit(0)
         else:
             out_path = os.path.join(in_path.parent, in_path.stem) + ".pdf"
@@ -40,13 +53,13 @@ def identify_path(in_path, out_path=None):
         paths['output'] = out_path
 
     # Condition when the path is a folder
-    else:
-        print("Path is a folder.")
+    elif os.path.isdir(in_path):
+        print("Identified input as a folder")
         paths['bulk'] = True
         paths['input'] = str(in_path)
 
         if out_path and os.path.isdir(out_path):
-            continue
+            pass
         elif out_path:
             print("Path does not exist")
             sys.exit(0)
@@ -54,6 +67,10 @@ def identify_path(in_path, out_path=None):
             out_path = str(in_path)
 
         paths['output'] = out_path
+    
+    else:
+        print("Please check your path and try again")
+        sys.exit(0)
 
     return paths
 
@@ -68,6 +85,7 @@ def main():
 
     args = parser.parse_args()
 
+    # print(args.outpath)
     paths = identify_path(args.inpath, args.outpath)
 
     convertToPdf(paths)
