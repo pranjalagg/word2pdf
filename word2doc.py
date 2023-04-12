4660082f1c18e0c204c7f54a12624ccfec18ab17
Pranjal Aggarwal <Pranjal.Aggarwal1@ibm.com>
2023-04-12 18:01:45 +0530

    Resolve Path when File

--- word2doc.py
+++ word2doc.py
@@ -1,6 +1,27 @@
+import argparse
+from pathlib import Path
+import os
+
+def identify_path(in_path, out_path=None):
+    # Resolve paths to handle relative paths
+    in_path = Path(in_path).resolve()
+    if out_path is not None:
+        out_path = Path(out_path).resolve()
+
+
+    dic = {}
+    if os.path.isfile(in_path):
+        print("Path is a file.")
+        dic['bulk'] = False
+        dic['input'] = str(in_path)
+
+        if out_path and os.path.isdir(out_path):
+            out_path = os.path.join(out_path, in_path.stem) + ".pdf"
+
+    else:
+        print("Path is a folder.")
+
 def main():
-    import textwrap
-    import argparse
 
     # Initialise parser
     parser = argparse.ArgumentParser(description="Program to convert word files to pdf via CLI")
